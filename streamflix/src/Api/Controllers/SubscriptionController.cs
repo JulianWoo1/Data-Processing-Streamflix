f2c59b304512a32c2aa9f4564eb725f20a58d289
Julian Woo <julian.woo@student.nhlstenden.com>
2025-11-27 15:35:09 +0100

    added subscription entity, subscriptionServices and start subscriptioncontroller

---
+++ streamflix/src/Api/Controllers/SubscriptionController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Streamflix.Api.DTOs;
+using Streamflix.Infrastructure.Data;
+using Streamflix.Infrastructure.Entities;
+
+namespace Streamflix.Api.Controllers;
+
+[ApiController]
+[Route("subscription")]
+public class SubscriptionController : ControllerBase
+{
+    private readonly ISubscriptionService _service;
+
+    public SubscriptionController(ISubscriptionService service)
+    {
+        _service = service;
+    }
+}
---
+++ streamflix/src/Infrastructure/Entities/subscription/Subscription.cs
@@ -0,0 +1,15 @@
+namespace Streamflix.Infrastructure.Entities;
+
+public class Subscription
+{
+    public int SubscriptionId { get; set; }
+    public int AccountId { get; set; }
+    public string SubscriptionType { get; set; }
+    public string SubscriptionDescription { get; set; }
+    public double BasePrice { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndTime { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsTrailPeriod { get; set; }
+    public DateTime TrailPeriodEnd { get; set; }
+}
